a8fa2ad38d3e1820dfe32163f4e02836a5edb1da
Josh Jurss <joshjurss@gmail.com>
2024-02-04 19:06:28 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -44,7 +44,7 @@ export const managers = [
     "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
   }
   {
-    "roster": 2  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+    "roster": 2,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     "managerID": "388932107793076224",
     "name": "Josh Jurss",
     "tookOver": null, // (optional) used if a manager took over a team, delete this line or change to null otherwise
